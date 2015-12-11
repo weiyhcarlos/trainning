d2eddbd8bda06438fd9ebe428cfd810f3fdd3a2a
carloswei <weiyh3@gmail.com>
2015-12-11 23:56:47 +0800

    update util.py

--- scripts/agent/util.py
+++ scripts/agent/util.py
@@ -1,31 +1,7 @@
 # -*- coding: UTF-8 -*-
 """util.py
 """
-import sys, importlib
-from ConfigParser import ConfigParser
-
-class GetConfig(object):
-    """配置读取类
-    """
-    def __init__(self, path):
-        self.config = ConfigParser()
-        self.config.read(path)
-
-    def get_section(self, section):
-        section_config = {}
-
-        options = self.config.options(section)
-        for option in options:
-            try:
-                section_config[option] = self.config.get(section, option)
-                if section_config[option] == -1:
-                    print "missing config on: %s" % option
-                    sys.exit(1)
-            except:
-                print "exception on %s!" % option
-                section_config[option] = None
-                sys.exit(1)
-        return section_config
+import importlib
 
 def str_to_class(module_name, class_name, *args):
     """动态实例化模块
