5bfaba254b4af0c58a208fd716e89b3d0f5b12a2
yutain2011 <conanpzh@gmail.com>
2015-12-14 22:48:17 +0800

    update logic

--- code/agent/agent.py
+++ code/agent/agent.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding=utf-8 -*-
 
-import time
+
 import server
 from utils.args_par import ArgsParse
 from utils.check_instance import CheckInstance
@@ -9,62 +9,104 @@ from utils.syn_time import syn_time
 from collector import Collector
 from handler import Handler
 from utils.config import Config
-import os
 import sys
 import time
+import threading
+import signal
 
 global_vars = {}  # add collect object
 PATH = "config.ini"
 
 
-def main():
-    """主方法
-    """
-    path = "%s/%s" % (sys.path[0], PATH)
-    # print path
-    config = Config(path)
-    result = config.run()
-    if result["status"] == 1:
-        return
-    ret = result["ret"]
-    global_vars["modules"] = ret["modules"]
-    global_vars["Handler"] = ret["Handler"]
-    global_vars["ttl"] = ret["ttl"]
-    global_vars["cluster"] = ret["cluster"]
-    paser = ArgsParse({"isExisted": 0, "version": '1.0'})
-    paras = paser.run()
-    # print paras
-    if "modules" in paras.keys():
-        global_vars["modules"] = paras["modules"]
-    if "ttl" in paras.keys():
-        global_vars["ttl"] = paras['ttl']
-    #print ret["modules"], ret["Handler"], ret["ttl"]
-    col_obj = Collector(global_vars["modules"])
-    hand_obj = Handler(global_vars["Handler"])
+def handler(signum, frame):
+    global global_vars
+    global_vars['isExited'] = True
+    while True:
+        if not global_vars['threadObj'].isAlive():
+            sys.exit(0)
+        time.sleep(1)
+
+
+def infos_func(global_vars):
+    col_obj = global_vars['collectObj']
+    hand_obj = global_vars['handlerObj']
     cycle_time = 1
     while True:
         print "**********", str(cycle_time) + " time(s) **********"
         start_time = time.time()
         info = col_obj.collect_info()
-        #收集失败,打印失败信息
+        # 收集失败,打印失败信息
         if info["status"] == 1:
             print info["ret"]
             time.sleep(global_vars["ttl"])
             continue
         print "successfully collect info!"
         info["ret"]["cluster"] = global_vars["cluster"]
-        handle_info = hand_obj.handle_data({"modules":global_vars["modules"],
-            "data":info["ret"]})
-        #处理失败,打印错误信息
+        handle_info = hand_obj.handle_data({"modules": global_vars["modules"],
+                                            "data": info["ret"]})
+        # 处理失败,打印错误信息
         if handle_info["status"] == 1:
             print handle_info["ret"]
-            if handle_info["ret"] == "fail to connect mongodb.":
-                exit(1)
-        else:
-            print "successfully handle data!"
+        print "successfully handle data!"
         print "--- %s seconds ---\n" % (time.time() - start_time)
+        print global_vars["ttl"]
+        print global_vars["modules"]
         time.sleep(global_vars["ttl"])
         cycle_time += 1
+        if global_vars["isExited"]:
+            break
+
+
+def main():
+    """主方法
+    """
+    signal.signal(signal.SIGINT, handler)
+    signal.signal(signal.SIGTERM, handler)
+    try:
+        path = "%s/%s" % (sys.path[0], PATH)
+        # print path
+        config = Config(path)
+        result = config.run()
+        if result["status"] == 1:
+            return
+        ret = result["ret"]
+        global global_vars
+        global_vars["modules"] = ret["modules"]
+        global_vars["Handler"] = ret["Handler"]
+        global_vars["ttl"] = ret["ttl"]
+        global_vars["cluster"] = ret["cluster"]
+        global_vars["port"] = ret["port"]
+        global_vars["isExited"] = False
+        is_existed = CheckInstance(global_vars["port"]).run()
+        # print "main is existed:%s" % is_existed
+        paser = ArgsParse({"isExisted": is_existed['status'], "port": global_vars[
+                          "port"], "version": '1.0'})
+        paras = paser.run()
+        # print paras
+        if "modules" in paras.keys():
+            global_vars["modules"] = paras["modules"]
+        if "ttl" in paras.keys():
+            global_vars["ttl"] = paras['ttl']
+        # print ret["modules"], ret["Handler"], ret["ttl"]
+        col_obj = Collector(global_vars["modules"])
+        hand_obj = Handler(global_vars["Handler"])
+        global_vars["collectObj"] = col_obj
+        global_vars['handlerObj'] = hand_obj
+        syn_time()  # check if ntpdate server install or not
+        # print global_vars['port']
+        # server_thread = threading.Thread(
+        #   target=server.start_server, args=(global_vars,))
+        # server_thread.setDaemon(True)
+        infos_thread = threading.Thread(target=infos_func, args=(global_vars,))
+        global_vars['threadObj'] = infos_thread
+        #print (global_vars['threadObj']).isAlive()
+        # print infos_thread.isAlive()
+        infos_thread.start()
+        server.start_server(global_vars)
+
+    except Exception, e:
+        print e
+        sys.exit(1)
 
 
 if __name__ == "__main__":
--- code/agent/utils/syn_time.sh
+++ code/agent/utils/syn_time.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
-chmod +x ./ntpdate_amd64.deb
-dpkg -i ./ntpdate_amd64.deb
+chmod +x ./utils/ntpdate_amd64.deb
 inf=$(cat /etc/crontab | grep ntpdate |wc -l)
 if [ "$inf" -eq 0 ];then
 	if [ `whoami` = "root" ];then
+		dpkg -i ./ntpdate_amd64.deb
    		echo "0 2 * * * root   ntpdate ntp.ubuntu.com" >> /etc/crontab
    	else
-    	sudo echo "0 2 * * * root   ntpdate ntp.ubuntu.com" >> /etc/crontab
+		sudo dpkg -i ./ntpdate_amd64.deb
+    		sudo echo "0 2 * * * root   ntpdate ntp.ubuntu.com" >> /etc/crontab
     fi
 fi
 ntpdate ntp.ubuntu.com
