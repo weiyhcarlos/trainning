b5275cd5ff8397009769bd632e0c389d743feff5
yutain2011 <conanpzh@gmail.com>
2015-12-15 10:27:27 +0800

    update syn time sh & add i386 deb

--- code/agent/utils/syn_time.sh
+++ code/agent/utils/syn_time.sh
@@ -1,13 +1,25 @@
 #! /bin/bash
 chmod +x ./utils/ntpdate_amd64.deb
+chmod +x ./utils/ntpdate_i386.deb
 inf=$(cat /etc/crontab | grep ntpdate |wc -l)
+is_x64=$(uname --m| grep 64|wc -l)
 if [ "$inf" -eq 0 ];then
 	if [ `whoami` = "root" ];then
-		dpkg -i ./ntpdate_amd64.deb
+		if [ "$is_x64" -eq 1 ];then
+			dpkg -i ./utils/ntpdate_amd64.deb
+		else
+			dpkg -i ./utils/ntpdate_i386.deb
+		fi
    		echo "0 2 * * * root   ntpdate ntp.ubuntu.com" >> /etc/crontab
+		ntpdate ntp.ubuntu.com
    	else
-		sudo dpkg -i ./ntpdate_amd64.deb
+		if [ "$is_x64" -eq 1 ];then
+                        sudo dpkg -i ./utils/ntpdate_amd64.deb
+                else
+                        sudo dpkg -i ./utils/ntpdate_i386.deb
+		fi
     		sudo echo "0 2 * * * root   ntpdate ntp.ubuntu.com" >> /etc/crontab
+		sudo ntpdate ntp.ubuntu.com
     fi
 fi
-ntpdate ntp.ubuntu.com
+#ntpdate ntp.ubuntu.com
