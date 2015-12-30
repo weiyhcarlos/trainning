25de99013836bd9dd24625ac00449ae9ac552677
carloswei <weiyh3@gmail.com>
2015-12-30 19:42:37 +0800

    add backgroud task

--- code/data_interface/start.sh
+++ code/data_interface/start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 cd /var/www/data_interface
 service nginx start
+celery worker -A web.celery --loglevel=DEBUG  -f /var/tmp/celery.log &
 uwsgi -i data_interface.ini 
-celery worker -A web.celery --loglevel=DEBUG  -f /var/tmp/celery.log
+
