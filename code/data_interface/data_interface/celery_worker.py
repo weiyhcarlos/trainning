bef4ba1f36a37a16f8191f1d055b6e0b52d0884c
carloswei <weiyh3@gmail.com>
2015-12-31 17:05:07 +0800

    update celery_wroker

--- code/data_interface/data_interface/celery_worker.py
+++ code/data_interface/data_interface/celery_worker.py
@@ -7,5 +7,5 @@ from app import upload_task
 #压入flask application context
 #celery中可以使用current_app获取到当前app对象
 
-# app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 # app.app_context().push()
