54810ef558e1a5a585ee0f48ffe98749e0afff4d
carloswei <carloswei@carlosweideMBP.lan>
2016-04-15 16:50:16 +0800

    update worker docker conf

--- agent-worker/misc/start.sh
+++ agent-worker/misc/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 cd /var/www/data_interface
 service nginx start
-celery worker -A celery_worker.celery --loglevel=DEBUG  -f /var/tmp/celery.log &
+celery worker -A code.celery_worker.celery --loglevel=DEBUG  -f /var/tmp/celery.log &
 uwsgi -i data_interface.ini
 
