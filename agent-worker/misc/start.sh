8495ce0bcd5be7442beb3fc418725bcff683cbe8
carloswei <carloswei@carlosweideMBP.lan>
2016-04-15 18:30:54 +0800

    add pythonpath

--- agent-worker/misc/start.sh
+++ agent-worker/misc/start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 cd /var/www/data_interface
 service nginx start
+export PYTHONPATH=conf:./:./code
 celery worker -A code.celery_worker.celery --loglevel=DEBUG  -f /var/tmp/celery.log &
 uwsgi -i data_interface.ini
 
